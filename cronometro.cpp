6e38fc22776602c523aa7a4f26943ac65b6bc4c8
Rossi-123 <133289912+Rossi-123@users.noreply.github.com>
2023-05-11 19:44:27 -0500

    Add files via upload

---
+++ cronometro.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int hf,mf,sf,hi,mi,si,ht,mt,st,s;
+cout <<"Ingrese el tiempo final (hf mf sf): ";
+cin >>hf>>mf>>sf;
+cout <<"Ingrese el tiempo inicial (hi mi si): ";
+cin >>hi>>mi>>si;
+if(sf>si){
+
+}else{
+mf=mf-1;
+sf=sf+60;
+}
+st=sf-si;
+if(mf>mi){
+
+}else{
+hf=hf-1;
+mf=mf+60;
+}
+mt=mf-mi;
+ht=hf-hi;
+s=ht*3600+mt*60+st;
+cout<<"Han transcurrido "<<ht<<" horas: "<<mt<<" Minutos: "<<st<<" Segundos: "<<endl;
+cout<<"Entonces en total son: "<<s<<" segundos"<<endl<<endl;
+return (0);
+}
